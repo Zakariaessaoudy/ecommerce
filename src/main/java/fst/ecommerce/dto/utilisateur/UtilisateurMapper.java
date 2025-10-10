c9f832beb69b37cc6e3604586e20f365abff803a
ahmeedYH <ahmedhanchouch99@gmail.com>
2025-10-10 22:23:25 +0100

    smthng

--- src/main/java/fst/ecommerce/dto/utilisateur/UtilisateurMapper.java
+++ src/main/java/fst/ecommerce/dto/utilisateur/UtilisateurMapper.java
@@ -9,14 +9,14 @@ import java.util.stream.Collectors;
 public interface UtilisateurMapper {
 
     // Entity → DTO
-    @Mapping(source = "wishlist.id", target = "wishListId")
-    @Mapping(source = "panier.id", target = "panierId")
-    @Mapping(target = "commandeIds", expression = "java(utilisateur.getCommandes() != null ? utilisateur.getCommandes().stream().map(c -> c.getId()).collect(Collectors.toList()) : null)")
+   // @Mapping(source = "wishlist.id", target = "wishListId")
+    //@Mapping(source = "panier.id", target = "panierId")
+    //@Mapping(target = "commandeIds", expression = "java(utilisateur.getCommandes() != null ? utilisateur.getCommandes().stream().map(c -> c.getId()).collect(Collectors.toList()) : null)")
     UtilisateurDto toDTO(Utilisateur utilisateur);
 
     // DTO → Entity
-    @Mapping(source = "wishListId", target = "wishlist.id")
-    @Mapping(source = "panierId", target = "panier.id")
-    @Mapping(target = "commandes", ignore = true) //  on ignore pour éviter boucle
+    //@Mapping(source = "wishListId", target = "wishlist.id")
+    //@Mapping(source = "panierId", target = "panier.id")
+    //@Mapping(target = "commandes", ignore = true) //  on ignore pour éviter boucle
     Utilisateur toEntity(UtilisateurDto dto);
 }
